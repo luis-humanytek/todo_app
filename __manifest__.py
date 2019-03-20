97d10cc3804297ff562b72a551a17d3a414bccfc
humanytek <luis-humanytek>
2019-03-20 10:46:50 -0600

    [ADD] Add files to module

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'To-Do Application',
+    'description': 'Manage your personal Tasks with this module.',
+    'author': 'Jose Luis Diaz',
+    'company': 'Humanytek',
+    'website': 'Humanytek.com',
+    'version': '12.0',
+    'depends': ['mail'],
+    'application': True,
+}
